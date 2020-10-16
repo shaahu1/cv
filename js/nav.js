d9fb689712f308d515e359827e15e08cb347fabd
shaahu1 <58043292+shaahu1@users.noreply.github.com>
2020-10-17 02:10:24 +0530

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,121 @@
+
+$(window).scroll(function() {
+	var top_of_element = $("#info").offset().top;
+	var bottom_of_element = $("#info").offset().top + $("#info").outerHeight();
+	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+	var top_of_screen = $(window).scrollTop();
+
+	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	    $("#link2").css({"color" : "#34EE91" , "font-size": "50px"});
+	    $("#link3").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link1").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link4").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link6").css({"color" : "#818181" , "font-size": "35px"});
+	} else {
+    		    
+	}
+});
+
+$(window).scroll(function() {
+	var top_of_element = $("#education").offset().top;
+	var bottom_of_element = $("#education").offset().top + $("#education").outerHeight();
+	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+	var top_of_screen = $(window).scrollTop();
+
+	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        $("#link3").css({"color" : "#34EE91" , "font-size": "50px"});
+        $("#link2").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link1").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link4").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link6").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link5").css({"color" : "#818181" , "font-size": "35px"});
+    } else {
+    		    
+    }
+});
+
+$(window).scroll(function() {
+	var top_of_element = $("#home").offset().top;
+	var bottom_of_element = $("#home").offset().top + $("#home").outerHeight();
+	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+	var top_of_screen = $(window).scrollTop();
+
+	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	    $("#link1").css({"color" : "#34EE91" , "font-size": "50px"});
+	    $("#link3").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link2").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link4").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link6").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link5").css({"color" : "#818181" , "font-size": "35px"});
+	} else {
+   		    
+	}
+});
+
+$(window).scroll(function() {
+	var top_of_element = $("#skills").offset().top;
+	var bottom_of_element = $("#skills").offset().top + $("#skills").outerHeight();
+	var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+	var top_of_screen = $(window).scrollTop();
+
+	if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	    $("#link4").css({"color" : "#34EE91" , "font-size": "50px"});
+	    $("#link1").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link3").css({"color" : "#818181" , "font-size": "35px"});
+	    $("#link2").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link6").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link5").css({"color" : "#818181" , "font-size": "35px"});
+    } else {
+    		    
+    }
+});
+
+$(window).scroll(function() {
+    var top_of_element = $("#contact").offset().top;
+    var bottom_of_element = $("#contact").offset().top + $("#contact").outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+	    $("#link6").css({"color" : "#34EE91" , "font-size": "50px"});
+        $("#link1").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link3").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link2").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link4").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link5").css({"color" : "#818181" , "font-size": "35px"});
+    } else {
+    	    
+    }
+});
+
+$(window).scroll(function() {
+    var top_of_element = $("#projects").offset().top;
+    var bottom_of_element = $("#projects").offset().top + $("#projects").outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)){
+        $("#link5").css({"color" : "#34EE91" , "font-size": "50px"});
+        $("#link6").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link1").css({"color" : "#818181" , "font-size": "35px"});
+        $("#link3").css({"color" : "#818181" , "font-size": "35px"});
+    	$("#link2").css({"color" : "#818181" , "font-size": "35px"});
+    	$("#link4").css({"color" : "#818181" , "font-size": "35px"});
+    } else {
+    		    
+    }
+});
+	
+
+
+
+
+
+
+
+        
+
+
+        /* popup4 */
+
+        
